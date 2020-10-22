15c607329095edcb884563f5eab8cb6e21facaad
Ivor <ivorchu@gmail.com>
2020-10-23 00:55:08 +0800

    Not Sure If This Is Right but the home page + reminder prob

--- __init__.py
+++ __init__.py
@@ -1,19 +1,20 @@
 #coding:UTF-8
 
-import tkinter as tk 
+import tkinter as tk
 from tkinter import * 
 from tkinter import font as tkFont
-from youtube import youtube
-from stock import stocks
+from tkinter import ttk
+#from youtube import youtube
+#from stock import stocks
 #from magnifier import activateMagnifier
-from lottery import main
-#from reminder import *
+#from lottery import main
+from reminder import *
 from news import refreshNews
 
 
 #Initialize Window
 window = tk.Tk()
-window.geometry("700x430")
+window.geometry("600x400")
 window.resizable(0, 0)
 window.title('Computer Assist Software') 
 font = tkFont.Font(family = "Arial", size = 12)
@@ -28,29 +29,34 @@ canvas = tk.Frame(window, height = 500, width = 600)
 canvas.pack()
 
 # clear frame (canvas)
-def clearFrame(frame):
-	for widget in frame.winfo_children():
-		widget.destroy()
-		frame.pack_forget()
-		frame.pack()
-	
+def clear_frame():
+	#canvas.destroy()
+	#canvas.pack_forget()
+	#canvas.grid_forget()
+	return 0
+
+def Home():
+	canvas = Canvas(window, width = 300, height = 300)      
+	canvas.pack()   
+	img = PhotoImage(file=r"profile_img.png")
+	canvas.create_image(20,20, anchor=NW, image=img)
 
 #Buttons
-btn_home = tk.Button(frame_opt, text='主畫面', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: [clearFrame(canvas)])
+btn_home = tk.Button(frame_opt, text='Home', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = Home)
 btn_home["font"] = font 
-btn_passw = tk.Button(frame_opt, text='帳號密碼管理', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: [clearFrame(canvas)])
+btn_passw = tk.Button(frame_opt, text='password', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center")
 btn_passw["font"] = font 
-btn_mag = tk.Button(frame_opt, text='Magnifier', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: activateMagnifier(font))
+btn_mag = tk.Button(frame_opt, text='Magnifier', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center")
 btn_mag["font"] = font 
-btn_stocks = tk.Button(frame_opt, text='股票', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: [clearFrame(canvas),stocks(canvas, font)])
+btn_stocks = tk.Button(frame_opt, text='Stocks', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: stocks(canvas, font))
 btn_stocks["font"] = font 
-btn_lottery = tk.Button(frame_opt, text='彩卷', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: [clearFrame(canvas),main(canvas, font)])
+btn_lottery = tk.Button(frame_opt, text='Lottery', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: main(canvas, font))
 btn_lottery["font"] = font 
-btn_remd = tk.Button(frame_opt, text='提醒 & 行事曆', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: [clearFrame(canvas),printAll(canvas, font)])
+btn_remd = tk.Button(frame_opt, text='Reminder', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: printAll(canvas, font))
 btn_remd["font"] = font 
-btn_music = tk.Button(frame_opt, text='音樂 & Youtube', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: [clearFrame(canvas), youtube(canvas, font)])
+btn_music = tk.Button(frame_opt, text='Youtube', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: youtube(canvas, font))
 btn_music["font"] = font 
-btn_news = tk.Button(frame_opt, text='新聞', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: [clearFrame(canvas),refreshNews(canvas, font)]) 
+btn_news = tk.Button(frame_opt, text='News', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: refreshNews(canvas, font)) 
 btn_news["font"] = font
 
 #Packs
