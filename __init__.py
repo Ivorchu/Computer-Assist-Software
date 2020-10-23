11f62c1e658cd3afaea356452bc7e01550bad7da
Ethan830 <ethany8300918@gmail.com>
2020-10-23 11:53:14 +0800

    passw still in progress

--- __init__.py
+++ __init__.py
@@ -4,8 +4,8 @@ import tkinter as tk
 from tkinter import * 
 from tkinter import font as tkFont
 from tkinter import ttk
-#from youtube import youtube
-#from stock import stocks
+from youtube import youtube
+from stock import stocks
 #from magnifier import activateMagnifier
 #from lottery import main
 from reminder import *
@@ -46,17 +46,17 @@ btn_home = tk.Button(frame_opt, text='Home', width=12, height=2, bd=0, bg = "#33
 btn_home["font"] = font 
 btn_passw = tk.Button(frame_opt, text='帳號密碼管理', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: [clearFrame(canvas), run(canvas, font)])
 btn_passw["font"] = font 
-btn_mag = tk.Button(frame_opt, text='放大鏡', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: activateMagnifier(font))
+btn_mag = tk.Button(frame_opt, text='放大鏡', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: [clearFrame(canvas),activateMagnifier(font)])
 btn_mag["font"] = font 
-btn_stocks = tk.Button(frame_opt, text='Stocks', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: stocks(canvas, font))
+btn_stocks = tk.Button(frame_opt, text='Stocks', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: [clearFrame(canvas),stocks(canvas, font)])
 btn_stocks["font"] = font 
-btn_lottery = tk.Button(frame_opt, text='Lottery', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: main(canvas, font))
+btn_lottery = tk.Button(frame_opt, text='Lottery', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: [clearFrame(canvas),main(canvas, font)])
 btn_lottery["font"] = font 
-btn_remd = tk.Button(frame_opt, text='Reminder', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: printAll(canvas, font))
+btn_remd = tk.Button(frame_opt, text='Reminder', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: [clearFrame(canvas),printAll(canvas, font)])
 btn_remd["font"] = font 
-btn_music = tk.Button(frame_opt, text='Youtube', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: youtube(canvas, font))
+btn_music = tk.Button(frame_opt, text='Youtube', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: [clearFrame(canvas),youtube(canvas, font)])
 btn_music["font"] = font 
-btn_news = tk.Button(frame_opt, text='News', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: refreshNews(canvas, font)) 
+btn_news = tk.Button(frame_opt, text='News', width=12, height=2, bd=0, bg = "#33383E", fg = "white", anchor = "center", command = lambda: [clearFrame(canvas),refreshNews(canvas, font)]) 
 btn_news["font"] = font
 
 #Packs
--- password.py
+++ password.py
@@ -1,9 +1,7 @@
-#from data.ahk import autohotkey
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 
-#autohotkey(
 
 def run(window, font):
 	ask = tk.Label(window, text = "請選擇輸入Google或Facebook帳戶及密碼")
@@ -13,43 +11,57 @@ def run(window, font):
 	combo.grid(row = 1, column = 1, pady = 5) 
 	combo.current(0)
 	result = ""
+	acc = tk.StringVar()
+	passw = tk.StringVar()
 	def setup():
-		acc = tk.StringVar()
 		account = tk.Entry(window, textvariable = acc)
 		account["font"] = font
 		account.insert(0, '帳號')
-		account.grid(row = 4, column = 1, pady = (10), padx = 160) 
-		acc = acc.get()
+		account.grid(row = 4, column = 1, pady = 10, padx = 160) 
 
-		passw = tk.StringVar()
 		password = tk.Entry(window, textvariable = passw)
 		password["font"] = font
 		password.insert(0, '密碼')
-		password.grid(row = 5, column = 1, pady = (10), padx = 160) 
-		passw = passw.get()
+		password.grid(row = 5, column = 1, pady = 10, padx = 160) 
 
 		btn = tk.Button(window, text='儲存', width=5, height=1, bd=0, bg = "#D35400", fg = "white")
 		btn["font"] = font
 		btn.grid(row = 6, column = 1, pady = 10, padx = 160)
+
+		
+
+	label = tk.Label(window, text = "")
+	label["font"] = font
 	def getResult():
-		if combo.get() == 'Google':
-			label = tk.Label(window, text = "請輸入您的Google帳戶及密碼")
-			label["font"] = font
+		if combo.get() == combo['values'][1]:
+			label['text'] = "請輸入您的Google帳戶及密碼"
 			label.grid(row = 3, column = 1, pady = 5) 
 			setup()
+			acc = acc.get()
+			passw = passw.get()
 			window.update() 
 			window.update_idletasks()
+			
 		elif combo.get() == combo['values'][2]:
-			label = tk.Label(window, text = "請輸入您的Facebook帳戶及密碼")
-			label["font"] = font
-			label.grid(row = 3, column = 1, pady = 10) 
+			label['text'] = "請輸入您的Facebook帳戶及密碼"
+			label.grid(row = 3, column = 1, pady = 5) 
 			setup()
+			acc = acc.get()
+			passw = passw.get()
 			window.update() 
 			window.update_idletasks()
+
+		def importData():
+			f = open("data.ahk", "w")
+			f.write(""+acc+"\t"+passw)
+			f.close()
+
+		importData()
 
 	confirm = tk.Button(window, text='確定', width=5, height=1, bd=0, bg = "#D35400", fg = "white", command = lambda: getResult())
 	confirm["font"] = font
 	confirm.grid(row = 2, column = 1, pady = 5, padx = 10)
-	
+
+
 
 	
